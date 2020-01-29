6ba12dd1c1acacb599b55749374eb87d07d10e90
Victor Abraham <vpa4565@gmail.com>
2020-01-29 12:40:26 -0700

    Build basic file architecture and some views/components

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Heading.scss';
+
+
+const Heading = () => {
+  return (
+    <header>
+      <div>
+        <h1>I'm a header</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Heading;
---
+++ src/views/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Heading from '../../components/Heading/Heading';
+import './Home.scss';
+
+class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+    <div>
+      <Heading/>
+    </div>
+    )
+  }
+}
+
+export default Home;
