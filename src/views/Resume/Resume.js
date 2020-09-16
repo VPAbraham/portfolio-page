1cfd20e13ba8e9a0fd54ee9298dd32e7d53dcd85
Victor Abraham <vpa4565@gmail.com>
2020-09-16 16:16:02 -0600

    Set up resume view

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Resume.scss';
 import { Link } from 'react-router-dom';
+import resumeImg from '../../assets/images/resume.jpg';
 
 const Resume = () => {
   return (
     <div className='resume'>
-      <p>resume here</p>
-      <Link to='/'>BACK</Link>
+      <Link to='/' className='resume-link'> GO BACK</Link>
+      <h1>Resume</h1>
+      <img src={resumeImg} />
     </div>
   )
 }
