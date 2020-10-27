8b38da898c05b7e03b732819853b4f3d26f27838
Victor Abraham <vpa4565@gmail.com>
2020-10-26 20:13:39 -0600

    Adjust spacing for about sections

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -9,14 +9,23 @@ const About = () => {
   return (
     <div className='about'>
       <Link to='/' className='resume-link about-back'>GO BACK</Link>
-      <section>
+      <section className='about-top'>
         <div className='about-block'>
           <h2>I design & build</h2>
           <h2>digital products</h2>
-          <div className='about-block__text'> 
-
+         
+        </div>
+        <img src={victorImg} alt='Headshot of Victor Abraham.'/>
+      </section>
+      <section className='about-bottom'>
+      <div className='about-bottom__text'> 
+            <p>After being invited to an internal internship at my IT job,
+               I decided to make a switch within the tech industry and took the leap of becoming 
+               a full-time development student through Turing. After a challenging and rewarding 8 month course, 
+               I have spent the better part of the past year working as a freelance developer and an online coding instructor. 
+                 When I'm not coding, I am a fantasy football commissioner, a doting dog father, organizer of Catan game nights, an occasional gym-goer, amateur writer, and an audiobook/podcast enthusiast. </p>
           </div>
-          <div className='about-block__contact'>
+        <div className='about-bottom__contact'>
             <p>email: 
               <a href='mailto:vpa456@gmail.com'>vpa456@gmail.com</a>
             </p>
@@ -29,8 +38,6 @@ const About = () => {
             </p>
             
           </div>
-        </div>
-        <img src={victorImg} alt='Headshot of Victor Abraham.'/>
       </section>
     </div>
   )
