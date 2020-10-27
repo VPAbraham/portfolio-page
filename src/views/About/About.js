3981267ce4744d0309113283e7a50e14c9ee0bea
Victor Abraham <vpa4565@gmail.com>
2020-10-26 19:41:21 -0600

    Add contact information and social links

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -10,9 +10,25 @@ const About = () => {
     <div className='about'>
       <Link to='/' className='resume-link about-back'>GO BACK</Link>
       <section>
-        <div>
+        <div className='about-block'>
           <h2>I design & build</h2>
           <h2>digital products</h2>
+          <div className='about-block__text'> 
+
+          </div>
+          <div className='about-block__contact'>
+            <p>email: 
+              <a href='mailto:vpa456@gmail.com'>vpa456@gmail.com</a>
+            </p>
+            <p>phone: 910.795.7233</p>
+            <p>github: 
+              <a href='https://github.com/VPAbraham' target='_blank'>github.com/VPAbraham</a>
+            </p>
+            <p>linkedIn: 
+              <a href='https://www.linkedin.com/in/victorpabraham/' target='_blank'>linkedin.com/in/victorpabraham/</a>
+            </p>
+            
+          </div>
         </div>
         <img src={victorImg} alt='Headshot of Victor Abraham.'/>
       </section>
