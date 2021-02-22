f6f20c64ab6c5ede3cc3781a7c1dc547c4fcd460
VPAbraham <vpa4565@gmail.com>
2021-02-22 11:22:40 -0700

    Create testimonials section and pull in testimonial screenshots from UpWork.

---
+++ src/components/TestimonialsSection/TestimonialsSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TestimonialsSection.scss';
+
+const TestimonialsSection = () => {
+
+  return(
+    <div>
+      <h2>TESTIMONIALS</h2>
+      <p>TESTES</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  )
+}
+export default TestimonialsSection;
